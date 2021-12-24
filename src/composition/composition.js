25ae35e806765ae28055b87c3ecebff46e781983
Alexander <Alexander_alatorre1993@hotmail.com>
2021-12-23 16:46:44 -0800

    added hoc

--- src/composition/composition.js
+++ src/composition/composition.js
@@ -1,25 +1,57 @@
-import logo from '../logo.svg';
-import '../App.css';
+import { customerA, customerB, customerC } from "./customerTypes";
+import "../App.css";
+import { useState } from "react";
 
 function Composition() {
+  const OrderReportWithComposition = withComposition(OrderReport);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <OrderReport customer={customerA}>I am your child</OrderReport>
+      <OrderReportWithComposition
+        customer={customerB}
+      ></OrderReportWithComposition>
     </div>
   );
 }
+
+function OrderReport({ customer, children }) {
+  const { name, address, total } = customer;
+
+  return (
+    <div>
+      <b>{name}</b>
+      <hr />
+      <span>{address}</span>
+      <br />
+      <span>Orders: {total}</span>
+      {children}
+    </div>
+  );
+}
+
+const withComposition = (BaseComponent) => (props) => {
+  const [fastTracker, setFastTracker] = useState(props.isFastTracked);
+
+  const toggleFastTracker = () => {
+    setFastTracker(!fastTracker);
+  };
+
+  const isFastrackerEnabled = fastTracker ? (
+    <div>Fast Tracked Enabled</div>
+  ) : (
+    <div>Not Fast Tracked</div>
+  );
+
+  const ChildrenOfBaseComponent = (
+    <BaseComponent customer={props.customer}>
+      <br />
+      <button onClick={toggleFastTracker}>Toggle Tracker</button>
+      {isFastrackerEnabled}
+    </BaseComponent>
+  );
+
+  return ChildrenOfBaseComponent;
+};
 
 export default Composition;
---
+++ src/composition/customerTypes.js
@@ -0,0 +1,22 @@
+const customerA = {
+  name: "Company A",
+  address: "720 Kennedy Rd",
+  total: 1000,
+};
+
+const customerB = {
+  name: "Company B",
+  address: "410 Ramsy St",
+  total: 1000,
+  isEligible: true,
+  isFastTracked: false,
+};
+
+const customerC = {
+  name: "Company C",
+  address: "123 Abram Ave",
+  total: 1010,
+  specialDelivery: true,
+};
+
+export { customerA, customerB, customerC };
