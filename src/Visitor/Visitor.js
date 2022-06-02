7bbc810d30e30eaa3f6af2d4698a645047229bc2
Alexander <Alexander_alatorre1993@hotmail.com>
2022-06-01 18:33:50 -0600

    added visitor component

---
+++ src/Visitor/Visitor.js
@@ -0,0 +1,105 @@
+import React from "react";
+import "./styles.css";
+
+const simpleFormSchema = [
+  {
+    label: "Name",
+    required: true,
+    fieldType: "Text",
+  },
+  {
+    label: "Birthdate",
+    required: true,
+    fieldType: "Date",
+  },
+  {
+    label: "Number of Pets",
+    required: false,
+    fieldType: "Number",
+  },
+];
+
+const formWithChildrenSchema = [
+  {
+    label: "Personal Details",
+    fieldType: "Section",
+    children: [
+      {
+        label: "Name",
+        fieldType: "Text",
+      },
+      {
+        label: "Birthdate",
+        fieldType: "Date",
+      },
+    ],
+  },
+  {
+    label: "Favorites",
+    fieldType: "Section",
+    children: [
+      {
+        label: "Favorite Movie",
+        fieldType: "Text",
+      },
+    ],
+  },
+];
+
+const defaultComponents = {
+  Text: ({ label }) => (
+    <label>
+      {label}
+      <input type="text" />
+    </label>
+  ),
+  Date: ({ label }) => (
+    <label>
+      {label}
+      <input type="date" />
+    </label>
+  ),
+  Number: ({ label }) => (
+    <label>
+      {label}
+      <input type="number" />
+    </label>
+  ),
+  Section: ({ label, children }) => (
+    <details>
+      <summary>{label}</summary>
+      {children}
+    </details>
+  ),
+};
+
+function ViewGenerator({ schema, components }) {
+  const mergedComponents = {
+    ...defaultComponents,
+    ...components,
+  };
+
+  return schema.map((field, index) => {
+    const children = field.children ? (
+      <ViewGenerator schema={field.children} components={mergedComponents} />
+    ) : null;
+
+    return (
+      <React.Fragment key={index}>
+        {mergedComponents[field.fieldType]({ ...field, children })}
+      </React.Fragment>
+    );
+  });
+}
+
+export default function App() {
+  return (
+    <>
+      <h2>Simple Form</h2>
+      <ViewGenerator schema={simpleFormSchema} />
+
+      <h2>Form With Children</h2>
+      <ViewGenerator schema={formWithChildrenSchema} />
+    </>
+  );
+}
