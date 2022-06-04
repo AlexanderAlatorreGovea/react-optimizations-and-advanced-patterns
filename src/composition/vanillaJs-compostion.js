beb35406df615e3e4784122fc3f299569d55b0d0
Alexander <Alexander_alatorre1993@hotmail.com>
2022-06-04 12:15:53 -0600

    limited chars to 67

--- src/composition/vanillaJs-compostion.js
+++ src/composition/vanillaJs-compostion.js
@@ -8,16 +8,18 @@ const double = (x) => x * 2;
 const square = (x) => x * x;
 const minusTwo = (x) => x - 2;
 
-var output_final = compose(minusTwo, square, double)(2);
+var output_final = compose(square, double)(2);
 console.log(output_final);
 
 // pipe
-
 const pipe =
   (...fns) =>
   (x) =>
     fns.reduce((y, f) => f(y), x);
 
-const pipedOutput = pipe(square, double, minusTwo, minusTwo, minusTwo)(2);
+const pipedOutput = pipe(
+  double,
+  square,
+)(2);
 
 console.log(pipedOutput);
